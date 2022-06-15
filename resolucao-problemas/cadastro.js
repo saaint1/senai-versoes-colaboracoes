72c1ea6942037c7ab757e0217f58410d2fd84403
saaint <pa.ulans10@hotmail.com>
2022-06-15 16:07:19 -0300

    fiz atividade online

--- resolucao-problemas/cadastro.js
+++ resolucao-problemas/cadastro.js
@@ -1,1 +1,21 @@
-console.log("Hello World"); 
+//se a peça possuir um peso superior a 100 gramas, pode cadastrar
+//lista de peças superior a 10 = capacidade insuficiente
+//nome com quantidade menor que 3 caracteres = erro
+
+let pesoPeca = 50; 
+let nomePecas = "para-choque"; 
+let listaQtdPecas = 9; 
+
+if(pesoPeca >= 100) {
+   console.log("Peça cadastrável"); 
+}else {
+   console.log("Peça não cadastrável"); 
+}
+
+if(listaQtdPecas >= 10){
+   console.log("Carrinho cheio"); 
+}
+
+ if(nomePecas.length < 3){
+    console.log("Erro: Nome inválido. Digite pelo menos 3 caracteres!"); 
+ }
